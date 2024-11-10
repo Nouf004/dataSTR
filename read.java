418d8866af14fe0c914e421ab220a2bde23fe1d2
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-11-10 15:28:27 +0300

    Update read.java

--- read.java
+++ read.java
@@ -3,34 +3,35 @@ import java.io.*;
 
 public class read {
 
-    public void Load(String fileName) {
-        String line = null;
-
-        try {
-            File f = new File(fileName);
-            Scanner s = new Scanner(f);
-
-            s.nextLine();
-
-            while (s.hasNextLine()) {
-                line = s.nextLine();
-
-                if (line.trim().length() < 3) {
-                    System.out.println("Empty line");
-                    break;
-                }
-                System.out.println(line);
-
-                String str = s.next();
-
-                s.close();
+   public void Load(String fileName) {
+      String line = null;
+   
+      try {
+         File f = new File(fileName);
+         Scanner s = new Scanner(f);
+      
+         s.nextLine();
+      
+         while (s.hasNextLine()) {
+            line = s.nextLine();
+         
+            if (line.trim().length() < 3) {
+               System.out.println("Empty line");
+               break;
             }
-        } catch (EOFException e) {
-            System.out.println("End of file");
-        } catch (Exception e) {
-            System.out.println("Error opening file");
-            System.exit(1);
-        }
-
-    }
+            System.out.println(line);
+         
+            String x=line.substring(0,line.indexOf(','));
+            int id= Integer.parseInt(x.trim()); //term for the whitespace
+            String text=line.substring(line.indexOf(',')+1).trim();
+         
+            s.close();
+         }
+      } catch (Exception e) {
+         System.out.println("Error opening file");
+         System.exit(1);
+      }
+   
+   }
 }
+//reding the file
