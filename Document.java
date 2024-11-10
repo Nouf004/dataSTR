fdbfdc06932d1a56f6c08780b7dde04bdfc7b67e
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-11-10 15:27:07 +0300

    Update and rename Document_ID.java to Document.java

--- Document.java
+++ Document.java
@@ -1,14 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-public class Document_ID {
+public class Document{
 
     int ID;
     DoubleLinkedList<String> List_ID = new DoubleLinkedList<>();
 
-    Document_ID(int id, DoubleLinkedList<String> Text) {
+    Document(int id, DoubleLinkedList<String> Text) {
         ID = id;
         List_ID = Text;
     }
 
 }
+//to take the id
