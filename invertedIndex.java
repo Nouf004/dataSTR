90276c7fd779d3903d9418361b749bef2b74cd76
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-11-11 14:58:33 +0300

    Update invertedIndex.java

--- invertedIndex.java
+++ invertedIndex.java
@@ -1,7 +1,34 @@
 import java.util.*;
-
 public class invertedIndex{
+   DoubleLinkedList<Word> inverList;
 
+   invertedIndex(){
+      inverList=new DoubleLinkedList<Word>();
+   }//const
 
+   void addWord(String str, int ID){
+      if(!search(str)){
+         Word w= new Word(str);
+         w.document_IDs.insert(ID);
+         inverList.insert(w);
+      }//end if
+      else{
+         Word existWord=inverList.retrieve();
+         existWord.addID(ID);
+      }
+   }//end method
 
+   boolean search(String word){
+      if(inverList==null||inverList.empty())
+         return false;
+      inverList.FindFirst();
+      while(!inverList.last()){
+         if(inverList.retrieve().text.equals(word))
+            return true;
+         inverList.FindNext();
+      }//while
+      if(inverList.retrieve().text.equals(word))
+         return true;
+      return false;
+   }//end method
 }
