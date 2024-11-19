270b2642b357c1f83bdfca989848b19d867c07e3
lamaldakheel <lamaldakhilallh@hotmail.com>
2024-11-19 15:18:31 +0300

    Add files via upload

--- StopWords.java
+++ StopWords.java
@@ -103,10 +103,8 @@ void loadAllFiles (String Sfile , String docFiles) {
  Load_Stop(Sfile);
  loadAllDocs(docFiles);
  }
-void displayDocWithID(LinkedList<Integer>id) {
-   stop.display();
-}
-   
+
+
 }//end of class
 
 // main?
