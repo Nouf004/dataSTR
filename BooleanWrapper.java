1d656c66e8317b625d542b74e0591423db87449f
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-11-19 21:42:18 +0300

    Add files via upload

---
+++ BooleanWrapper.java
@@ -0,0 +1,15 @@
+public class BooleanWrapper {
+    private boolean value;
+
+    public BooleanWrapper(boolean value) {
+        this.value = value;
+    }
+
+    public boolean get() {
+        return value;
+    }
+
+    public void set(boolean value) {
+        this.value = value;
+    }
+}
