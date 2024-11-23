d74f267f123cf0feead5596673a77aa82b5b284d
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-11-23 19:08:56 +0300

    Update InvertedIndexBST.java

--- InvertedIndexBST.java
+++ InvertedIndexBST.java
@@ -1,9 +1,9 @@
 public class InvertedIndexBST {
 
-   BST<Word> inverted_index;
+   BST<Word> inverted_indexbst;
 
    public InvertedIndexBST() {
-      inverted_index = new BST<Word>();
+      inverted_indexbst = new BST<Word>();
 
    }
 
@@ -14,13 +14,13 @@ public class InvertedIndexBST {
       inverted.inverList.FindFirst();
       while (!inverted.inverList.last()) {
 
-         inverted_index.insert(inverted.inverList.retrieve().text, inverted.inverList.retrieve());
+         inverted_indexbst.insert(inverted.inverList.retrieve().text, inverted.inverList.retrieve());
 
          inverted.inverList.FindNext();
 
       } // while
 
-      inverted_index.insert(inverted.inverList.retrieve().text, inverted.inverList.retrieve());
+      inverted_indexbst.insert(inverted.inverList.retrieve().text, inverted.inverList.retrieve());
 
    }// end add from list
 
@@ -28,27 +28,27 @@ public class InvertedIndexBST {
       if (!search(str)) {
          Word w = new Word(str);
          w.document_IDs.insert(ID);
-         inverted_index.insert(str, w);
+         inverted_indexbst.insert(str, w);
       } // end if
       else {
-         Word existWord = inverted_index.retrieve();
+         Word existWord = inverted_indexbst.retrieve();
          existWord.addID(ID);
       }
    }// end method
 
    boolean search(String word) {
-      return inverted_index.findkey(word);
+      return inverted_indexbst.findkey(word);
    }// end method
 
    void displayInvertedIndex() {
 
-      if (inverted_index == null) {
+      if (inverted_indexbst == null) {
          System.out.println("null");
          return;
-      } else if (inverted_index.empty()) {
+      } else if (inverted_indexbst.empty()) {
          System.out.println("it is empty");
          return;
       }
-      inverted_index.inOrder();
+      inverted_indexbst.inOrder();
    }
 } // end class
